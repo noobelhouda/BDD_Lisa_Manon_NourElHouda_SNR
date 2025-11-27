27e14bcf5bbc76cde790f78b210bbd95f561156e
zazouu9 <lisa.cazoulat@icloud.com>
2025-11-27 08:40:58 +0100

    QS15 - fill parent frame

--- gui/playground.py
+++ gui/playground.py
@@ -11,7 +11,24 @@ from gui_config import configure_style
 
 
 ###################### You'll copy here the code to play with ######################
+window = tk.Tk()
+window.title("Playground")
 
+
+configure_style()
+
+
+first_frame = ttk.Frame(window, style="Sample.TFrame")
+first_label = ttk.Label(first_frame, text="First label", style="Sample.TLabel")
+first_ent_var = tk.StringVar(value="")
+first_text_field = ttk.Entry(first_frame, textvariable=first_ent_var)
+first_button = ttk.Button(first_frame, text="First button")
+first_label.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+
+first_text_field.pack()
+first_button.pack()
+first_frame.pack(expand=True, fill=tk.BOTH)
+window.mainloop()
 
 
 #####################################################################################
